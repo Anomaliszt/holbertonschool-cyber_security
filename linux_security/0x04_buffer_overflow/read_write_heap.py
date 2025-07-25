0ad4d85d8e227aa7a13b6dd900cff124042433a1
Anomaliszt <xavier.sanchez44@orange.fr>
2025-07-25 09:57:28 +0200

    Add read_write_heap.py script for heap memory manipulation

---
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+This script reads and writes to the heap of a process
+"""
+import sys
+
+def find_and_replace_string(pid, search_string, replace_string):
+    """Finds a string in the heap of a process and replaces it."""
+    try:
+        with open(f"/proc/{pid}/maps", "r") as maps_file:
+            for line in maps_file:
+                if "[heap]" in line:
+                    heap_range = line.split()[0]
+                    heap_start = int(heap_range.split('-')[0], 16)
+                    heap_end = int(heap_range.split('-')[1], 16)
+                    break
+
+        with open(f"/proc/{pid}/mem", "r+b") as mem_file:
+            mem_file.seek(heap_start)
+            data = mem_file.read(heap_end - heap_start)
+            if search_string.encode() in data:
+                new_data = data.replace(search_string.encode(), replace_string.encode())
+                mem_file.seek(heap_start)
+                mem_file.write(new_data)
+
+    except:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: read_write_heap.py pid search_string replace_string")
+        sys.exit(1)
+
+    pid = sys.argv[1]
+    search_string = sys.argv[2]
+    replace_string = sys.argv[3]
+
+    find_and_replace_string(pid, search_string, replace_string)
