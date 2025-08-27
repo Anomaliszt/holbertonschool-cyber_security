d80039436225e65e5c21ca3ff7ff90a2394dcbb5
Anomaliszt <xavier.sanchez44@orange.fr>
2025-08-27 12:37:33 +0200

    Refactor JSON merging function for improved readability and efficiency

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -3,13 +3,14 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  file1_content = File.read(file1_path)
-  file2_content = File.read(file2_path)
-  
-  data1 = JSON.parse(file1_content)
-  data2 = JSON.parse(file2_content)
+  data1 = JSON.parse(File.read(file1_path))
+  data2 = JSON.parse(File.read(file2_path))
 
   merged_data = data2 + data1
 
-  File.write(file2_path, JSON.pretty_generate(merged_data))
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged_data))
+  end
+
+  puts "Merged JSON written to #{file2_path}"
 end
